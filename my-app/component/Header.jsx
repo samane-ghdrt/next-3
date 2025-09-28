a5185e18e18a99eedbd7c5252cb726894b03a4ec
samane ghodrati <samane.ghodrati@gmail.com>
2025-09-28 00:05:21 -0900

    add

---
+++ my-app/component/Header.jsx
@@ -0,0 +1,105 @@
+"use client";
+import * as React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
+import {AppBar, Box, Button, CssBaseline, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography} from '@mui/material';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+
+const drawerWidth = 240;
+const navItems = [
+    {href:"/",title:'Home'},
+    {href:"/posts",title:'posts'},
+    {href:"/recipes",title:'recipes'},
+    {href:"/users",title:'users'}];
+
+function Header() {
+    const path=usePathname();
+//   const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+        <Link href={item.href} key={item.title} style={{ color: "black", textDecoration: "none" }}>
+          <ListItem disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.title} />
+            </ListItemButton>
+          </ListItem>
+          </Link>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            MUI
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) =>{
+                const activestyle={textDecoration:path===item.href?"underline overline" : "none"}
+            return (
+
+              <Button key={item} sx={{ color: '#fff' }}>
+              <Link href={item.href} style={{ ...activestyle, color: "white" }}>
+                {item}
+                </Link>
+              </Button>
+          
+            )}
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+}
+
+export default Header;
---
+++ my-app/not-found.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function notfound() {
+  return (
+    <div>
+    <Typography mt={10} variant="h3" textAlign="center">
+    This race is not found in the lord of the rings world
+  </Typography>
+  </div>
+  )
+}
