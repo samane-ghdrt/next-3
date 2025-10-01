04763b8bc26d3f866cec58e8788ecf7ca0ea948a
samane <samane.ghodrati@gmail.com>
2025-10-01 03:45:48 -0700

    add

---
+++ my-app/component/Card.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Card,CardContent,CardMedia,Typography,Button,CardActionArea,CardActions} from '@mui/material';
+
+
+export default function MultiActionAreaCard({member}) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={member.image}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
