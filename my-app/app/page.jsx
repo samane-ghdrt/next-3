07716ec3562815418c26f8080a094d7f6e88ece6
samane <samane.ghodrati@gmail.com>
2025-10-02 02:32:49 -0700

    add

--- my-app/app/page.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import { getData} from "@/utils/getData"
-export default async function Home() {
-  return (
-    <>
-  
-    </>
-  );
-}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { getData} from "@/utils/getData"
+export default async function Home() {
+  return (
+   <div>
+  const users=getData("https://dummyjson.com/users");
+  const posts=getData("https://dummyjson.com/posts');
+  const recipes=getData("https://dummyjson.com/recipes");
+</div>
+
+  
+  )
+}
