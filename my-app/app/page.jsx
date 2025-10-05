f2fc5d01ce1709eacd95e9ceefe924c3f417c89a
samane <samane.ghodrati@gmail.com>
2025-10-04 22:37:43 -0700

    add

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -1,9 +1,11 @@
+import React from 'react'
 import Image from "next/image";
-import { getData} from "@/utils/getData"
-export default async function Home() {
+// import { getData} from "@/utils/getData"
+export default function Home() {
   return (
-   <div>
-  const users=getData("https://dummyjson.com/users");
+<div>
+  
+  const users=agetData("https://dummyjson.com/users");
   const posts=getData("https://dummyjson.com/posts');
   const recipes=getData("https://dummyjson.com/recipes");
 </div>
