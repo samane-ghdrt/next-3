8e06fbb7b6543b632f315f59d9f4247e2a2cddd9
samane <samane.ghodrati@gmail.com>
2025-10-13 03:42:32 -0700

    add

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -5,13 +5,13 @@ export default async function Home() {
   const usersData = await getData("https://dummyjson.com/users");
   const postsData = await getData("https://dummyjson.com/posts");
   const recipesData = await getData("https://dummyjson.com/recipes");
-  console.log(usersData.users);
+  console.log(postsData.posts);
   
   return (
 <div>
 <ListI items={usersData?.users || []}/>
-<ListI items={postsData?.users || []}/>
-<ListI items={recipesData?.users || []}/>
+<ListI items={postsData?.posts || []}/>
+<ListI items={recipesData?.recipes || []}/>
 </div>
   )
 }
